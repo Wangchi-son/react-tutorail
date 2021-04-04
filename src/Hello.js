55a4fd1a38e1ada386d0b19e562cdee519cdc884
unknown <sonwj0915@naver.com>
2021-04-04 22:14:03 +0900

    조건부 렌더링

--- src/Hello.js
+++ src/Hello.js
@@ -1,13 +1,15 @@
 import React from "react";
 
-function Hello({ color, name }) {
+function Hello({ color, name, isSpecial }) {
   return (
     <>
       <div
         style={{
-          color: color,
+          color,
         }}
       >
+        {isSpecial ? <b>*</b> : null}
+        {isSpecial && <b>*</b>}
         안녕하세요 {name}
       </div>
     </>
