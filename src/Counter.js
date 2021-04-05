54ded24142e93011cf9b2446bdbd16e62d3e7960
unknown <sonwj0915@naver.com>
2021-04-05 11:02:58 +0900

    useState 업데이트 함수

--- src/Counter.js
+++ src/Counter.js
@@ -5,18 +5,14 @@ function Counter() {
   // 배열의 첫 번째 인자는 상태, 두번째 인자는 바꿔주는 함수, useState내부의 값은 기본값
   const [number, setNumber] = useState(0);
 
-  //원래라면
-  //const numberState = useState(0);
-  //const number = numberState[0];
-  //const setNumber = numberState[1];
-  //이런 식으로 적어야 하지만 배열 비구조화 할당을 통해 간소화
-
   const onIncrease = () => {
-    setNumber(number + 1);
+    // 업데이트 함수
+    // 현재 상태를 가져와서 업데이트 하겠다(prevNumber는 이름이 뭐가되든 상관없음)
+    setNumber((prevNumber) => prevNumber + 1);
   };
 
   const onDecrease = () => {
-    setNumber(number - 1);
+    setNumber((prevNumber) => prevNumber - 1);
   };
 
   return (
