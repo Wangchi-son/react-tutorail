77f2bc4a4028b3aeddbc1187a5a8ef822a44df55
unknown <sonwj0915@naver.com>
2021-04-05 11:34:28 +0900

    Input rest버튼

--- src/InputSample.js
+++ src/InputSample.js
@@ -2,16 +2,19 @@ import React, { useState } from 'react';
 
 function InputSample() {
   const [text, setText] = useState('');
-  // 여기서 (e)는 컴포넌트 내에서 수정 이벤트가 발생했을 때 이벤트에 대한 내용이 이벤트 객체에 파라미터로 받아와서 사용
-  // e.target은 현재 이벤트가 발생한 곳에대한 정보
-  // e.target.value는 현재 이벤트가 발생한 곳의 값
+
   const onChange = (e) => {
     setText(e.target.value);
   };
+
+  const onReset = () => {
+    setText('');
+  };
+
   return (
     <div>
       <input onChange={onChange} value={text} />
-      <button>초기화</button>
+      <button onClick={onReset}>초기화</button>
       <div>
         <b>값: </b>
         {text}
