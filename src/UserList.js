b8b03e84bec31cca6e5a04750d6bbaab7f0027f0
unknown <sonwj0915@naver.com>
2021-04-05 20:31:09 +0900

    deps 배열을 추가한 형태

--- src/UserList.js
+++ src/UserList.js
@@ -4,19 +4,13 @@ function User({ user, onRemove, onToggle }) {
   const { username, email, id, active } = user;
 
   useEffect(() => {
-    console.log('컴포넌트가 화면에 나타남');
-    // 마운트 될 때
-    // props로 받은 값을 state로 설정하는 것
-    // 외부 API 요청, REST API를 요청할 때
-    // D3, Video.js 등등 라이브러리를 사용하게 될 때
-    // setInterval, setTimeout 같은 작업도 사용
+    console.log('user 값이 설정됨');
+    console.log(user);
     return () => {
-      // 언마운트 될 때
-      // clearInterval, clearTimeout 같은 작업
-      // 라이브러리 인스턴스 제거
-      console.log('컴포넌트가 화면에서 사라짐');
+      console.log('user 값이 바뀌기 전');
+      console.log(user);
     };
-  }, []);
+  }, [user]);
 
   return (
     <div>
