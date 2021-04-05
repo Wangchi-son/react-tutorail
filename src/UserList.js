e3264ceaea4bb7b8ee54021fc2d1df49c6559464
unknown <sonwj0915@naver.com>
2021-04-05 19:41:44 +0900

    배열에 항목 수정하기

--- src/UserList.js
+++ src/UserList.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
-function User({ user, onRemove }) {
-  const { username, email, id } = user;
+function User({ user, onRemove, onToggle }) {
+  const { username, email, id, active } = user;
   return (
     <div>
-      <b>{username}</b> <span>({email})</span>
+      <b style={{ color: active ? 'green' : 'black', cursor: 'pointer' }} onClick={() => onToggle(id)}>
+        {username}
+      </b>{' '}
+      <span>({email})</span>
       {/* 새로운 함수를 on____에 호출하는 법 */}
       <button onClick={() => onRemove(id)}>삭제</button>
       {/* onClick={onRemove(id)}로 실행할 경우 렌더와 동시에 모두 삭제됨 */}
@@ -12,11 +15,11 @@ function User({ user, onRemove }) {
   );
 }
 
-function UserList({ users, onRemove }) {
+function UserList({ users, onRemove, onToggle }) {
   return (
     <div>
       {users.map((user) => (
-        <User user={user} key={user.id} onRemove={onRemove} />
+        <User user={user} key={user.id} onRemove={onRemove} onToggle={onToggle} />
       ))}
     </div>
   );
