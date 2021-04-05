92bdce11c128df2480ba739ad9b5c56c76ba202a
unknown <sonwj0915@naver.com>
2021-04-05 20:09:46 +0900

    useEffect를 사용하여 마운트 언마운트 업데이트 시 할 작업 설정

--- src/UserList.js
+++ src/UserList.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 function User({ user, onRemove, onToggle }) {
   const { username, email, id, active } = user;
+
+  useEffect(() => {
+    console.log('컴포넌트가 화면에 나타남');
+    // 마운트 될 때
+    // props로 받은 값을 state로 설정하는 것
+    // 외부 API 요청, REST API를 요청할 때
+    // D3, Video.js 등등 라이브러리를 사용하게 될 때
+    // setInterval, setTimeout 같은 작업도 사용
+    return () => {
+      // 언마운트 될 때
+      // clearInterval, clearTimeout 같은 작업
+      // 라이브러리 인스턴스 제거
+      console.log('컴포넌트가 화면에서 사라짐');
+    };
+  }, []);
+
   return (
     <div>
       <b style={{ color: active ? 'green' : 'black', cursor: 'pointer' }} onClick={() => onToggle(id)}>
