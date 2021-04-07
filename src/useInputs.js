b544fde6c200de6c084f8c3ae049834142491aba
unknown <sonwj0915@naver.com>
2021-04-08 00:39:32 +0900

    useInputs 커스텀 훅을 useReducer를 활용하여 만들기

--- src/useInputs.js
+++ src/useInputs.js
@@ -1,14 +1,34 @@
-import { useState, useCallback } from 'react';
+import { useReducer, useCallback } from 'react';
 
-//커스텀 Hook을 만들 때 이름에는 use{구현하고자 하는 기능 이름}으로 제작
+function reducer(state, action) {
+  switch (action.type) {
+    case 'CHANGE_INPUT':
+      return {
+        ...state,
+        [action.name]: action.value
+      };
+    case 'RESET':
+      return Object.keys(state).reduce((acc, current) => {
+        acc[current] = '';
+        return acc;
+      }, {});
+    default:
+      return state;
+  }
+}
+
 function useInputs(initialForm) {
-  const [form, setForm] = useState(initialForm);
+  const [form, dispatch] = useReducer(reducer, initialForm);
+  //change
   const onChange = useCallback((e) => {
     const { name, value } = e.target;
-    setForm((form) => ({ ...form, [name]: value }));
+    dispatch({
+      type: 'CHANGE_INPUT',
+      name,
+      value
+    });
   }, []);
-  const reset = useCallback(() => setForm(initialForm), [initialForm]);
-
+  const reset = useCallback(() => dispatch({ type: 'RESET' }), []);
   return [form, onChange, reset];
 }
 
