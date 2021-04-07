e91990cc78b3906c38bd0d0b1304efc4d2b97a46
unknown <sonwj0915@naver.com>
2021-04-07 19:30:24 +0900

    useReducer 기초

--- src/Counter.js
+++ src/Counter.js
@@ -1,18 +1,31 @@
-import React, { useState } from "react";
+import React, { useReducer } from 'react';
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+    default:
+      throw new Error('Unhandled action');
+    // or
+    // state
+  }
+}
 
 function Counter() {
-  // useState 사용법
-  // 배열의 첫 번째 인자는 상태, 두번째 인자는 바꿔주는 함수, useState내부의 값은 기본값
-  const [number, setNumber] = useState(0);
+  const [number, dispatch] = useReducer(reducer, 0);
 
   const onIncrease = () => {
-    // 업데이트 함수
-    // 현재 상태를 가져와서 업데이트 하겠다(prevNumber는 이름이 뭐가되든 상관없음)
-    setNumber((prevNumber) => prevNumber + 1);
+    dispatch({
+      type: 'INCREMENT'
+    });
   };
 
   const onDecrease = () => {
-    setNumber((prevNumber) => prevNumber - 1);
+    dispatch({
+      type: 'DECREMENT'
+    });
   };
 
   return (
