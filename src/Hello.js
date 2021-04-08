b31730b7e97907493ae18ffa785920a8313f7eb5
unknown <sonwj0915@naver.com>
2021-04-08 16:05:42 +0900

    클래스형 컴포넌트

--- src/Hello.js
+++ src/Hello.js
@@ -1,22 +1,38 @@
-import React from "react";
+import React from 'react';
 
-function Hello({ color, name, isSpecial }) {
-  return (
-    <>
-      <div
-        style={{
-          color,
-        }}
-      >
-        <b>{isSpecial ? "스페셜하다!" : "낫스페셜"}</b>
+class Hello extends React.Component {
+  static defaultProps = {
+    name: '이름없음'
+  };
+  render() {
+    const { color, isSpecial, name } = this.props;
+    return (
+      <div style={color}>
+        {isSpecial && <b>*</b>}
         안녕하세요 {name}
       </div>
-    </>
-  );
+    );
+  }
 }
 
-Hello.defaultProps = {
-  name: "이름없음",
-};
+// 함수형 컴포넌트
+// function Hello({ color, name, isSpecial }) {
+//   return (
+//     <>
+//       <div
+//         style={{
+//           color,
+//         }}
+//       >
+//         <b>{isSpecial ? "스페셜하다!" : "낫스페셜"}</b>
+//         안녕하세요 {name}
+//       </div>
+//     </>
+//   );
+// }
+
+// Hello.defaultProps = {
+//   name: '이름없음'
+// };
 
 export default Hello;
